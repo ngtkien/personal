5be3fd6383df47bceef59e2a3166f0d9d8cc67c2
kienngo <kien.ngo@fuelcloud.com>
2023-08-31 17:43:07 +0700

    - seperate ble module

---
+++ esp32s3/include/bluetooth_adv.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void bluetooth_init();
+
+static void	bas_notify(void);
+
+static void	hrs_notify(void);
---
+++ esp32s3/src/bluetooth_adv.c
@@ -0,0 +1,114 @@
+
+#include <errno.h>
+#include <stddef.h>
+#include <string.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/services/bas.h>
+#include <zephyr/bluetooth/services/hrs.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/types.h>
+#include "bluetooth_adv.h" // Include the bluetooth.h header file
+static const struct bt_data		ad[] = {BT_DATA_BYTES(BT_DATA_FLAGS,
+			(BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+			BT_DATA_BYTES(BT_DATA_UUID16_ALL,
+			BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
+			BT_UUID_16_ENCODE(BT_UUID_BAS_VAL),
+			BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))};
+
+static void	connected(struct bt_conn *conn, uint8_t err)
+{
+	if (err)
+	{
+		printk("Connection failed (err 0x%02x)\n", err);
+	}
+	else
+	{
+		printk("Connected\n");
+	}
+}
+
+static void	disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	printk("Disconnected (reason 0x%02x)\n", reason);
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
+static void	bt_ready(void)
+{
+	int	err;
+
+	printk("Bluetooth initialized\n");
+	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err)
+	{
+		printk("Advertising failed to start (err %d)\n", err);
+		return ;
+	}
+	printk("Advertising successfully started\n");
+}
+
+static void	auth_cancel(struct bt_conn *conn)
+{
+	char	addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+	printk("Pairing cancelled: %s\n", addr);
+}
+
+static struct bt_conn_auth_cb	auth_cb_display = {
+	.cancel = auth_cancel,
+};
+
+static void	bas_notify(void)
+{
+	uint8_t	battery_level;
+
+	battery_level = bt_bas_get_battery_level();
+	battery_level--;
+	if (!battery_level)
+	{
+		battery_level = 100U;
+	}
+	bt_bas_set_battery_level(battery_level);
+}
+
+static void	hrs_notify(void)
+{
+	static uint8_t	heartrate;
+
+	heartrate = 90U;
+	/* Heartrate measurements simulation */
+	heartrate++;
+	if (heartrate == 160U)
+	{
+		heartrate = 90U;
+	}
+	bt_hrs_notify(heartrate);
+}
+// ...
+void bluetooth_init()
+{
+    int err;
+    // Bluetooth initialization code
+    // ...
+    err = bt_enable(NULL);
+	if (err)
+	{
+		printk("Bluetooth init failed (err %d)\n", err);
+		return (0);
+	}
+	bt_ready();
+	bt_conn_auth_cb_register(&auth_cb_display);
+}
+// ...
